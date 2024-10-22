f6abc29622a3313e714af573db50cdd368eb58c5
ukn0wme <ukn0wme.design@gmail.com>
2024-10-23 01:57:14 +0500

    Initial Commit

---
+++ ass6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ass6
+//
+//  Created by Daniyal Nurgazinov on 23.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ass6Tests/ass6Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ass6Tests.swift
+//  ass6Tests
+//
+//  Created by Daniyal Nurgazinov on 23.10.2024.
+//
+
+import Testing
+@testable import ass6
+
+struct ass6Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
